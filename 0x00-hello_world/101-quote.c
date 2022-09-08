dbaa97cff06b4b8f660b990ff2a1a15fc70dd014
Amber Ozenogu <amberozenogu@gmail.com>
2022-09-08 08:15:12 -0700

    q

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
-* main -Entry point
-* Return:1
+* main - Entry point
+* Return: 1
 */
 int main(void)
 {
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1, ch, sizeof(ch));
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
