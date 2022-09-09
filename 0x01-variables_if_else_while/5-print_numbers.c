00967e3806736a7d600012feff122e7efc79a357
Amber Ozenogu <amberozenogu@gmail.com>
2022-09-09 07:19:38 -0700

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdlib.h>
 /**
- * maim - main block
+ * main - main block
  * Description: prints all single digit numbers of base 10 starting from 0
  * Return: 0
  */
