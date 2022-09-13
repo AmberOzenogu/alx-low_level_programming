1ae4189538f07e87082bfca4ed80c72ac1a38651
Amber Ozenogu <amberozenogu@gmail.com>
2022-09-13 06:34:24 -0700

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,12 +5,11 @@
  */
 int main(void)
 {
-int i =0;
-char put[] = "_putchar";
-while (i < 9)
+char *sh = "_putchar";
+while (*sh)
 {
-_putchar(put[i]);
-i++;
+_putchar(*sh);
+sh++;
 }
 _putchar('\n');
 return (0);
