c03ceaf03df1e81f64a82b0414d3679f4b95899d
Amber Ozenogu <amberozenogu@gmail.com>
2022-09-15 01:45:31 -0700

    main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,14 @@
 #include "main.h"
+#include <time.h>
 /**
  * main - prints positive or negative integer
  * Return: 0
  */
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
+int n;
+srand(time(0)); 
+n = rand() - RAND_MAX / 2;
+positive_or_negative(n);
 return (0);
 }
