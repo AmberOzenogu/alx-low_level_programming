cbd5d0736a7e80fe6f0b2144a20a5fa54b30aaea
Amber Ozenogu <amberozenogu@gmail.com>
2022-09-22 01:22:11 -0700

    num

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * print_number - prints an integer
- * @n: integer to print
+ * @n: the integer to print
  * Return: 0
- **/
+ */
 void print_number(int n)
 {
 if (n < 0)
@@ -11,10 +11,7 @@ if (n < 0)
 _putchar('-');
 n *= -1;
 }
-
 if (n / 10)
-{
 print_number(n / 10);
-}
 _putchar((n % 10) + '0');
 }
