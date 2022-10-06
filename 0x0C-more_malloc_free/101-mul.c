f9ef7a46c117e46ea7f022cad6f69ce54f7fb0f1
Amber Ozenogu <amberozenogu@gmail.com>
2022-10-06 08:32:19 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,110 +1,119 @@
 #include "main.h"
 /**
- * adding_all_mul - sum all the addition to know the multiplication result
- * @a: number 1
- * @len_a: lenght of number 1
- * @b: number 2
- * @len_b: lenght of number 2
- * Return: Addition pointer to the total resul of the  multiplication
+ * main - multiplies two positive numbers
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0
  */
-add_t *adding_all_mul(char *a, int len_a, char *b, int len_b)
+int main(int argc, char *argv[])
 {
-add_t *result = NULL;
-int i = 0, j = 0, carry = 0;
-result = malloc(sizeof(add_t));
-result->next = NULL, result->n_dig = 0, result->len_r = len_a + len_b;
-result->n_add = malloc(sizeof(char) * result->len_r);
-for (i = 0; i < result->len_r; i++)
-result->n_add[i] = '0';
-for (i = len_a - 1; i >= 0; i--)
+char *f = argv[1];
+char *s = argv[2];
+if (argc != 3 || !onlyNumbers(f) || !onlyNumbers(s))
 {
-carry = 0;
-for (j = len_b - 1; j >= 0; j--)
-{
-carry += (a[i] - '0') * (b[j] - '0');
-carry += result->n_add[i + j + 1] - '0';
-result->n_add[i + j + 1] = (carry % 10) + '0';
-carry /= 10;
-}
-if (carry)
-result->n_add[i + j + 1] = (carry % 10) + '0';
-}
-if (result->n_add[0] != '0')
-result->n_dig = len_a + len_b;
-else
-result->n_dig = len_a + len_b - 1;
-return (result);
-}
-/**
- * print_free_result - print the result of the multiplication and free all
- * @result: Addition pointer to the total resul of the  multiplication
- * Result: Nothing
- */
-void print_free_result(add_t *result)
-{
-int i = 0, start_n = 0;
-i = 0;
-while (i < result->n_dig)
-{
-if (start_n || result->n_add[result->len_r - result->n_dig + i] != '0')
-{
-_putchar(result->n_add[result->len_r - result->n_dig + i]);
-start_n = 1;
-}
-i++;
-}
-if (!result->n_dig || !start_n)
-_putchar('0');
-_putchar('\n');
-free(result->n_add);
-free(result);
-}
-/**
- * error_message - print an error message and exit with status 98
- * Return: Nothing
- */
-void error_message(void)
-{
-char error_msg[] = "Error";
-int i = 0;
-while (error_msg[i] != '\0')
-{
-_putchar(error_msg[i]);
-i++;
-}
-_putchar('\n');
+printf("Error\n");
 exit(98);
 }
-/**
- * main - multiply 2 long numbers
- * usage <> ./mul num1 num2
- * @ac: number of arguments
- * @av: list of arguments
- * Return: 0 on success, another number otherwise
- */
-int main(int ac, char **av)
-{
-char *a = NULL, *b =  NULL;
-int i = 0, len_a = 0, len_b = 0, is_a = 1, is_b = 1, len_r = 0;
-add_t *result = NULL;
-if (ac != 3)
-error_message();
-for (i = 0, a = av[1], b = av[2]; is_a == 1 || is_b == 1; i++)
-{
-if (is_a == 1 && a[i] == '\0')
-is_a = 0, len_a = i;
-if (is_b == 1 && b[i] == '\0')
-is_b = 0, len_b = i;
-if ((is_a == 1 && (a[i] < '0' || a[i] > '9')) ||
-(is_b == 1 && (b[i] < '0' || b[i] > '9')))
-error_message();
-}
-if (len_a == 0 || len_b == 0)
-error_message();
-len_r = len_a + len_b;
-if (len_a > len_b)
-a = av[2], b = av[1], len_a = len_b, len_b = len_r - len_b;
-result = adding_all_mul(a, len_a, b, len_b);
-print_free_result(result);
+if (*f == 48 || *s == 48)
+printf("0\n");
+else
+multiply(s, f);
 return (0);
 }
+/**
+ * multiply - multiplies two numbers and displays it
+ * @f: first "number"
+ * @s: second "number"
+ */
+void multiply(char *f, char *s)
+{
+int i, len1, len2, total, fdigit, sdigit, res = 0, tmp;
+int *ptr;
+len1 = _strlen(f);
+len2 = _strlen(s);
+tmp = len2;
+total = len1 + len2;
+ptr = _calloc(sizeof(int), (len1 + len2));
+for (len1--; len1 >= 0; len1--)
+{
+fdigit = f[len1] - '0';
+res = 0;
+len2 = tmp;
+for (len2--; len2 >= 0; len2--)
+{
+sdigit = s[len2] - '0';
+res += ptr[len2 + len1 + 1] + (fdigit *sdigit);
+ptr[len1 + len2 + 1] = res % 10;
+res /= 10;
+}
+if (res)
+ptr[len1 + len2 + 1] = res % 10;
+}
+while (*ptr == 0)
+{
+ptr++;
+total--;
+}
+for (i = 0; i < total; i++)
+printf("%i", ptr[i]);
+printf("\n");
+}
+/**
+ * onlyNumbers - determines if string has only numbers
+ * @c: input string
+ * Return: 0 if false, 1 if true
+ */
+int onlyNumbers(char *c)
+{
+while (*c)
+{
+if (*c < '0' || *c > '9')
+return (0);
+c++;
+}
+return (1);
+}
+/**
+ * _strlen - returns the length of a string
+ * @s: string s
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+char *p = s;
+while (*s)
+s++;
+return (s - p);
+}
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: bytes of the memory area
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+char *ptr = s;
+while (n--)
+*s++ = b;
+return (ptr);
+}
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of elements of pointer
+ * @size: size of each member
+ * Return: pointer of allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *ptr;
+if (!nmemb || !size)
+return (NULL);
+ptr = malloc(size * nmemb);
+if (!ptr)
+return (NULL);
+_memset(ptr, 0, size * nmemb);
+return (ptr);
+}
+
